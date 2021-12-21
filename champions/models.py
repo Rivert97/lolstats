ba49a1d2e0973a3881ab53ecd98d9dafe477499c
Roberto García <betogarcia97@live.com.mx>
2021-12-20 23:39:48 -0600

    - Se crea tabla con campeones. - Se comienza con el desarrollo del frontend

---
+++ champions/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+import json
+import os
+
+DATA_DIR = "/home/rgarcia/Documents/projects/lolstats/data_dragon/11.23.1/data/es_MX/champion"
+
+class Champion(models.Model):
+    """Campeon del juego.
+
+    Esta clase guarda sus atributos basicos de identificacion,
+    los detalles se guardan en un archivo JSON.
+    """
+    identifier = models.CharField(max_length=20)
+    key = models.CharField(max_length=4)
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        """Devuelve la descripcion del objeto."""
+        return self.name
+
+    def get_data(self):
+        """Devuelve los datos guardados en formato JSON del campeon."""
+        filepath = DATA_DIR + "/" + self.name + ".json"
+        rawdata = {}
+        if os.path.isfile(filepath):
+            with open(filepath, 'r') as f:
+                rawdata = json.load(f)
+        data = rawdata.get('data', {})
+        return data.get(self.name, {})
---
+++ champions/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Champion
+
+def index(request):
+    """Muestra la lista de campeones disponibles."""
+    champions = Champion.objects.order_by('name')
+    context = {
+                'champions': champions,
+            }
+    return render(request, 'champions/index.html', context)
+
+def detail(request, champion_identifier):
+    """Muestra la pagina de detalles de un campeon."""
+    champion = Champion.objects.get(identifier=champion_identifier)
+    data = champion.get_data()
+    context = {
+                'champion': champion,
+                'data': data,
+            }
+    return render(request, 'champions/detail.html', context)
