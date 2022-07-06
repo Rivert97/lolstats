9b005b5094f86a742d3fec34cd0f36b185fcaeb9
Roberto García <betogarcia97@live.com.mx>
2022-07-05 21:46:18 -0500

    Se agrega primera aproximación (No sirve de nada)

--- scripts/ann.py
+++ scripts/ann.py
@@ -1,40 +1,43 @@
+import sys
 import numpy as np
 from tensorflow import keras
 from tensorflow.keras import layers
+import sklearn.model_selection
+
+datafile = "/datos/lolstats/datasets/final.csv"
+truthfile = "/datos/lolstats/datasets/final_truth.csv"
 
 # Model / data parameters
-num_classes = 10
-input_shape = (28, 28, 1)
+num_classes = 2
+input_shape = (80,)
 
-# the data, split between train and test sets
-(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+# Get the data
+print("Loading dataset...")
+x = np.genfromtxt(datafile, delimiter=',')
+y = np.genfromtxt(truthfile, delimiter=',')
 
-# Scale images to the [0, 1] range
-x_train = x_train.astype("float32") / 255
-x_test = x_test.astype("float32") / 255
-# Make sure images have shape (28, 28, 1)
-x_train = np.expand_dims(x_train, -1)
-x_test = np.expand_dims(x_test, -1)
+# split between train and test sets
+print("Splitting dataset...")
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.20)
+
 print("x_train shape:", x_train.shape)
 print(x_train.shape[0], "train samples")
 print(x_test.shape[0], "test samples")
 
-
 # convert class vectors to binary class matrices
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
+print("y_train shape:", y_train.shape)
 
 # Create the model
 model = keras.Sequential(
     [
         keras.Input(shape=input_shape),
-        layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
-        layers.MaxPooling2D(pool_size=(2, 2)),
-        layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
-        layers.MaxPooling2D(pool_size=(2, 2)),
-        layers.Flatten(),
+        layers.Dense(160, activation="relu"),
+        layers.Dense(160, activation="relu"),
+        layers.Dense(80, activation="relu"),
         layers.Dropout(0.5),
-        layers.Dense(num_classes, activation="softmax"),
+        layers.Dense(num_classes, activation="sigmoid"),
     ]
 )
 
@@ -42,14 +45,17 @@ model.summary()
 
 
 # Train the model
+print("Training model...")
 batch_size = 128
-epochs = 15
+epochs = 100
 
 model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 
 model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.1)
 
 # Evaluate the model
+print("Evaluating model...")
+
 score = model.evaluate(x_test, y_test, verbose=0)
 print("Test loss:", score[0])
 print("Test accuracy:", score[1])
